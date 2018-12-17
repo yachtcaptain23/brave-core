b435ccd45daf8d1f4a430dc2388f162b89ed0c11
Brian R. Bondy <netzen@gmail.com>
2018-12-17 15:09:08 -0500

    temp

--- app/brave_main_delegate_browsertest.cc
+++ app/brave_main_delegate_browsertest.cc
@@ -6,19 +6,20 @@
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/browser/domain_reliability/service_factory.h"
-#include "components/domain_reliability/service.h"
+//#include "chrome/browser/domain_reliability/service_factory.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/common/web_preferences.h"
 
 using BraveMainDelegateBrowserTest = InProcessBrowserTest;
 
+/*
 IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DomainReliabilityServiceDisabled) {
   EXPECT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kDisableDomainReliability));
   ASSERT_EQ(domain_reliability::DomainReliabilityServiceFactory::GetForBrowserContext(
         ((content::BrowserContext *)browser()->profile())), nullptr);
 }
+*/
 
 IN_PROC_BROWSER_TEST_F(BraveMainDelegateBrowserTest, DisableHyperlinkAuditing) {
   EXPECT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
